49318bbfd4776748a9a39bb2e4710bb8885bc493
Eric Hagemann <hagemann.eric@gmail.com>
2015-12-16 19:53:55 -0500

    First commit

---
+++ octal.rb
@@ -0,0 +1,23 @@
+class Octal
+  attr_reader :octal
+  
+  EXCEPTIONS = 'az89'
+  BASE = 8
+
+  def initialize(string)
+    if string.downcase.count(EXCEPTIONS) > 0
+        @octal = [0]
+    else
+        @octal = string.chars.reverse!.map { |s| s.to_i }
+    end
+  end
+
+  def to_decimal
+    dec_array = []
+    octal.each_index do |digit|
+
+      dec_array << octal[digit] * BASE ** digit
+    end
+    dec_array.reduce(:+)
+  end
+end
